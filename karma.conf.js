c995ff27d05b6f016a94f3134eb9f84471564382
Petyo Ivanov <underlog@gmail.com>
2016-03-08 17:52:04 +0200

    fix: Run Chrome in Travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -46,7 +46,14 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: [ 'Chrome' ],
+        browsers: [ 'TravisCI' ],
+
+        customLaunchers: {
+            TravisCI: {
+                base: "Chrome",
+                flags: [ '--no-sandbox' ]
+            }
+        },
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
