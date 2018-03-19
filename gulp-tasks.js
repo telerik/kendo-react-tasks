8598b3ec9cb70303be1ae43f6fe35eefb170128b
arizanov <arizanov@progress.com>
2018-03-19 14:08:38 +0200

    new cdn

--- gulp-tasks.js
+++ gulp-tasks.js
@@ -1,17 +1,18 @@
 "use strict";
 
-const path = require('path');
-const fs = require('fs');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
 const cdnWebpackConfig = require('./cdn.webpack.config.js');
+const _ = require('lodash');
 
 /* eslint-disable no-console */
 module.exports = (gulp, libraryName, compilerPath, basePath, options = {}) => { // eslint-disable-line max-params
     gulp.task('build-cdn', () => {
         const config = Object.assign({}, cdnWebpackConfig);
         config.output.filename = libraryName + '.js';
-        // TODO:
-        config.output.library = 'KendoReactGrid';
-    
+        // The library name is in pascal case.
+        config.output.library = _.flow(_.camelCase, _.upperFirst)(libraryName);
+
         return gulp.src('src/main' + ".{jsx,ts,tsx,js}")
             .pipe(webpackStream(config, webpack))
             .pipe(gulp.dest('dist/cdn/js'));
