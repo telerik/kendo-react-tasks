d154c5213b830d94feb47a084bbfe89eefdaaabe
arizanov <arizanov@progress.com>
2018-03-22 15:36:00 +0200

    split the e2e tasks

--- gulp-tasks.js
+++ gulp-tasks.js
@@ -5,6 +5,7 @@ const deepAssign = require('lodash.merge');
 const apiConfig = require('./api.conf.js');
 const registerStartTask = require('./tasks/start');
 const registerDocsTasks = require('./tasks/docs');
+const registerE2ETasks = require('./tasks/e2e');
 const docsServer = require('@telerik/kendo-common-tasks/docs-server');
 const lintSlugsTask = require('@telerik/kendo-common-tasks/lint-slugs');
 const apiTasks = require('@progress/kendo-typescript-tasks/api');
@@ -12,6 +13,7 @@ const apiTasks = require('@progress/kendo-typescript-tasks/api');
 module.exports = (gulp, libraryName, compilerPath, basePath, options = {}) => {
     registerDocsTasks(gulp, libraryName, options);
     registerStartTask(gulp, libraryName);
+    registerE2ETasks(gulp);
     lintSlugsTask(gulp);
     apiTasks(gulp, deepAssign({}, apiConfig, options.apiConfig));
 };
