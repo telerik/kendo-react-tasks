a2c2d8530d96926732a725ff28280ed7c3135d68
arizanov <arizanov@progress.com>
2018-03-28 17:02:58 +0300

    fix(systemjs): move args into obj

--- tasks/systemjs.js
+++ tasks/systemjs.js
@@ -6,7 +6,7 @@ const webpackConfig = require('./../webpack/systemjs.config');
 const systemjsTask = require('@telerik/kendo-common-tasks/systemjs-bundle/task');
 
 module.exports = function registerBundleSystemJSTask(gulp, libraryName, options) {
-    systemjsTask(gulp, { distName: libraryName, modules: options.modules, webpackConfig }, webpackStream, webpack);
+    systemjsTask(gulp, { distName: libraryName, modules: options.modules, webpackConfig, webpackStream, webpack });
 };
 
 
